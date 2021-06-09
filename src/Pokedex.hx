3097d5d3d90b41bf32b0d232148c605b429fcc68
Kino  Rose <kinoarose@gmail.com>
2021-06-09 19:33:16 -0400

    Update pokedex API information

--- src/Pokedex.hx
+++ src/Pokedex.hx
@@ -5,7 +5,7 @@ import Structs.PokedexOptions;
 @:jsRequire('pokedex-promise-v2')
 @:native('Pokedex')
 extern class Pokedex {
-	public function new(options:PokedexOptions);
+	public function new(?options:PokedexOptions);
 
 	public function getPokemonByName(name:String):Promise<Pokemon>;
 	public function getPokemonSpeciesByName(species:String):Promise<PokemonSpecies>;
