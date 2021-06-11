9edcef45e17b6fee61db0cc96d4328c16a18a585
DanielJarnutowski <37672099+DanielJarnutowski@users.noreply.github.com>
2021-06-11 17:08:47 -0400

    Dev branch (#6)
    * update package.json
    
    * update package.json
    
    * added more pokedex options
    
    * fixed merge conflict
    
    Co-authored-by: Kino  Rose <kinoarose@gmail.com>

--- src/Pokedex.hx
+++ src/Pokedex.hx
@@ -5,7 +5,6 @@ import js.lib.Promise;
 @:native('Pokedex')
 extern class Pokedex {
 	public function new(?options:PokedexOptions);
-
 	public function getPokemonByName(name:String):Promise<Pokemon>;
 	public function getPokemonSpeciesByName(species:String):Promise<PokemonSpecies>;
 	public function getPokemonsList():Promise<PokemonList>;
@@ -16,6 +15,25 @@ extern class Pokedex {
 	public function getEncounterConditionByName(condition:String):Promise<EncounterConditions>;
 	public function getEncounterConditionValueByName(condVal:String):Promise<EncounterConditionValue>;
 	public function getLanguageByName(lang:String):Promise<NameResource>;
+	public function getEvolutionChainById(id:Int):Promise<EvolutionChain>;
+	public function getEvolutionTriggerByName(evolutionTrigger:String):Promise<EvolutionTrigger>;
+	public function getVersionByName(version:String):Promise<Version>;
+	public function getVersionGroupByName(versionGroup:String):Promise<VersionGroup>;
+	public function getItemByName(item:String):Promise<Item>;
+	public function getItemAttributeByName(itemAttribute:String):Promise<ItemAttribute>;
+	public function getItemCategoryByName(itemCategory:String):Promise<ItemCategory>;
+	public function getItemFlingEffectByName(itemFlingEffect:String):Promise<ItemFlingEffect>;
+	public function getItemPocketByName(itemPocket:String):Promise<ItemPocket>;
+	public function getLocationByName(location:String):Promise<Location>;
+	public function getLocationAreaByName(locationArea:String):Promise<LocationArea>;
+	public function getPalParkAreaByName(palParkArea:String):Promise<PalParkArea>;
+	public function getRegionByName(region:String):Promise<Region>;
+	public function getAbilityByName(ability:String):Promise<Ability>;
+	public function getCharacteristicById(id:Int):Promise<Characteristic>;
+	public function getEggGroupByName(eggGroup:String):Promise<EggGroup>;
+	public function getGenderByName(gender:String):Promise<Gender>;
+	public function getGrowthRateByName(growthRate:String):Promise<GrowthRate>;
+	public function getNatureByName(nature:String):Promise<Nature>;
 	public function getMachineById(id:Int):Promise<Machine>;
 	public function getMoveByName(name:String):Promise<Move>;
 	public function getMoveAilmentByName(name:String):Promise<MoveAilment>;
